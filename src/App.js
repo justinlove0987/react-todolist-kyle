802fa03caf7c76781f41e1620f0da546ec9dd268
justinlove0987 <justinlove0987@gmail.com>
2021-12-05 16:25:02 +0800

    useRef && handleAddTodo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import TodoList from "./TodoList";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState(["1", "2"]);
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  }
+
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed</button>
       <div>0 left to do</div>
     </>
