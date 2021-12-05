75b2587d9b2ff6e6e674734617809939f236df78
justinlove0987 <justinlove0987@gmail.com>
2021-12-05 16:08:49 +0800

    set html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import TodoList from "./TodoList";
+
 function App() {
-  return null;
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
