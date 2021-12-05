54fbf78ec649ad355dc3682bb52847dea7bea8f4
justinlove0987 <justinlove0987@gmail.com>
2021-12-05 16:28:36 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import TodoList from "./TodoList";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (setTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
