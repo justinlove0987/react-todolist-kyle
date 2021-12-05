735baf16f130a9c5a6ad30863832f2a4f3ffa0ae
justinlove0987 <justinlove0987@gmail.com>
2021-12-05 16:39:27 +0800

    add hangdleClearTodos function

--- src/App.js
+++ src/App.js
@@ -33,12 +33,17 @@ function App() {
     todoNameRef.current.value = null;
   }
 
+  function hangdleClearTodos() {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
+
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed</button>
+      <button onClick={hangdleClearTodos}>Clear Completed</button>
       <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
     </>
   );
